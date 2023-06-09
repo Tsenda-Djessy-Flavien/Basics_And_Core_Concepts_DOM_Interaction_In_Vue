69d391974fe9b7c17707f72e95923549715cb76b
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-09 12:53:41 +0100

    Understanding 'methods' in Vue Apps

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = createApp({
        vueLink: 'https://vuejs.org/',
     }
    },
+   methods: {
+    outputGoal() {
+        const numberRandom = Math.random();
+        if (numberRandom < 0.5) {
+            return 'Learn Vue'
+        } else {
+            return 'Master Vue'
+        }
+    }
+   },
 })
 
 app.mount('#user-goal');
